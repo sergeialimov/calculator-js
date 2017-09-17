3088c9b0881addbe42fe144102a224a8be33200d
sergeialimov <sv.alimov@yandex.ru>
2017-09-17 21:10:27 +0300

    add css and js

---
+++ js/calc.js
@@ -0,0 +1,51 @@
+
+const calc = {
+  success: function() {
+  alert('success')
+  },
+
+  addMethod: function(name, funct) {
+    calc.name = funct;
+  },
+
+  add: function(a, b) {
+    return a + b
+  },
+
+  substract: function(a, b) {
+    return a - b
+  },
+
+  divide: function(a, b) {
+    return a / b
+  },
+
+  multiply: function(a, b) {
+    return a * b
+  },
+
+  calc: function(str) {
+    const inputRow = document.getElementById("input").value;
+    const inputs = inputRow.split(" ");
+    const firstNum = parseFloat(inputs[0]);
+    const secondNum = parseFloat(inputs[2]);
+    let result;
+    switch (inputs[1]) {
+      case '-':
+        result = calc.substract(firstNum, secondNum);
+        break;
+      case '+':
+        result = calc.add(firstNum, secondNum);
+        break;
+      case '*':
+        result = calc.multiply(firstNum, secondNum);
+        break;
+      case '/':
+        result = calc.divide(firstNum, secondNum);
+        break;
+      default:
+        result = 'input is incorrect'
+    }
+    document.getElementsByName('result')[0].value = result;
+  }
+}
