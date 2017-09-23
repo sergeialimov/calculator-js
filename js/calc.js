4f5a9b99fae78bd46742949f61be3281db82db45
sergeialimov <sv.alimov@yandex.ru>
2017-09-23 11:49:17 +0300

    add functions to display result, add numbers and functions

--- js/calc.js
+++ js/calc.js
@@ -1,50 +1,82 @@
 const calc = {
-  success: function() {
-    alert('success')
+  tmp: '',
+  firstNum: '',
+  secondNum: '',
+  action: '',
+  sum: '',
+
+  showResult: function(result) {
+    document.getElementsByName('result')[0].value = result;
   },
 
-  addMethod: function(name, funct) {
-    calc.name = funct;
+  addNumber: function(number) {
+    if (number === 0 && calc.tmp === '0') {
+      return;
+    }
+    if (calc.tmp === '0') {
+      calc.tmp = '';
+    }
+    calc.tmp += number;
+    console.log('number:', calc.tmp);
+    calc.showResult(calc.tmp);
+  },
+
+  addAction: function(action) {
+    if (!calc.firstNum) {
+      calc.firstNum = calc.tmp;
+      calc.tmp = '';
+      calc.action = action;
+    } else {
+      calc.secondNum = calc.tmp;
+      calc.parseNums;
+      switch (action) {
+        case '-':
+          result = calc.substract(firstNum, secondNum);
+          break;
+        case '+':
+          result = calc.add(firstNum, secondNum);
+          break;
+        case '*':
+          result = calc.multiply(firstNum, secondNum);
+          break;
+        case '/':
+          result = calc.divide(firstNum, secondNum);
+          break;
+      }
+    }
+    console.log('func', calc.action);
+    calc.showResult(calc.action);
+  },
+
+  parseNums: function() {
+    firstNum = parseFloat(firstNum);
+    secondNum = parseFloat(secondNum);
   },
 
   add: function(a, b) {
-    return a + b
+    calc.sum = a + b;
+    console.log('sum', calc.sum);
+    calc.showResult(calc.sum);
   },
 
   substract: function(a, b) {
-    return a - b
+    calc.sum = a - b;
+    calc.showResult(calc.sum);
   },
 
   divide: function(a, b) {
-    return a / b
+    calc.sum = a / b;
+    calc.showResult(calc.sum);
   },
 
   multiply: function(a, b) {
-    return a * b
+    calc.sum = a * b;
   },
 
-  calc: function(str) {
-    const inputRow = document.getElementById("input").value;
-    const inputs = inputRow.split(" ");
-    const firstNum = parseFloat(inputs[0]);
-    const secondNum = parseFloat(inputs[2]);
-    let result;
-    switch (inputs[1]) {
-      case '-':
-        result = calc.substract(firstNum, secondNum);
-        break;
-      case '+':
-        result = calc.add(firstNum, secondNum);
-        break;
-      case '*':
-        result = calc.multiply(firstNum, secondNum);
-        break;
-      case '/':
-        result = calc.divide(firstNum, secondNum);
-        break;
-      default:
-        result = 'input is incorrect'
-    }
-    document.getElementsByName('result')[0].value = result;
+  clear: function() {
+    calc.tmp = '0';
+    calc.action = '';
+    console.log('number:', calc.tmp);
+    calc.showResult(0);
   }
 }
