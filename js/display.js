2b690869d0c6ef6d57d7d499887c99895e0612e5
sergeialimov <sv.alimov@yandex.ru>
2017-09-25 13:32:38 +0300

    temporary remove all files to make PR

--- js/display.js
+++
@@ -1,9 +0,0 @@
-const display = {
-  showResult(result) {
-    document.getElementsByName('result')[0].value = result;
-  },
-
-  showError() {
-    document.getElementsByName('result')[0].value = 'Error';
-  }
-}
